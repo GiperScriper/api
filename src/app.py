c8ff0bc226a5ff4d6a5b1c12b7f36d0d0a20a062
Nikolay Martynov <tty0sh@gmail.com>
2016-03-10 22:22:46 +0300

    add db connection

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://localhost/api'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 from routes.index import *
 
--- src/routes/index.py
+++ src/routes/index.py
@@ -1,4 +1,6 @@
-from app import app
+from flask import request
+from app import app, db
+from models.user import User
 import json
 
 @app.route('/')
@@ -8,3 +10,17 @@ def index():
 @app.route('/hello/<message>/<message2>/')
 def hello(message, message2):
     return 'Hello %s, %s' % (message, message2)
+
+@app.route('/users', methods=['GET', 'POST'])
+def create_user():
+    data = json.loads(request.data.decode('utf-8'))
+    user = User(data['name'])
+    db.session.add(user)
+    db.session.commit()
+
+    return json.dumps(
+        {
+            'test': 'test message',
+            'data': data
+        }
+    )
